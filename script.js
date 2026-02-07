c342130505a018c6df645e1d11fa2027962cdf50
Enes Utku Cakmak <cakmakenesutku@gmail.com>
2026-02-07 22:04:48 +0300

    Navbar ve galeri düzenlemeleri eklendi

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 window.addEventListener("click", () => {
     const music = document.getElementById("bgMusic");
-    if (music) {
-        music.play().catch(() => {
-            // Tarayıcı otomatik çalmayı engellerse sessizce geç
-        });
+    if (music && music.paused) {
+        music.play();
     }
-});
+}, { once: true });
