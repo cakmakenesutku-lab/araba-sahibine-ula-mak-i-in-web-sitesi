d5268c4b97ef5a17e0cb8dc061d2dd2f06e22340
Enes Utku Cakmak <cakmakenesutku@gmail.com>
2026-02-27 23:30:38 +0300

    Siteyi güncelledim, yeni tasarım eklendi

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-window.addEventListener("click", () => {
+// Arka plan müziğini kullanıcı etkileşiminden sonra başlat
+window.addEventListener(
+  "click",
+  () => {
     const music = document.getElementById("bgMusic");
     if (music && music.paused) {
-        music.play();
+      music
+        .play()
+        .catch(() => {
+          // tarayıcı engellerse sessizce yoksay
+        });
     }
-}, { once: true });
+  },
+  { once: true }
+);
